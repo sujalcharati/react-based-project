44e899b30da7537e730d3e207b5daeb5f97d013a
sujalcharati <sujalc024@gmail.com>
2024-09-24 12:24:37 +0530

    settodos

--- todos-01/src/App.jsx
+++ todos-01/src/App.jsx
@@ -1,35 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+
+    title:"study",
+    description:" go through some command line..."
+  },
+  {
+
+    title:" time pass",
+    description:" play chess ..."
+  }
+  ]
+  
+)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <Buttoncomponent/>
+  <button onClick={()=>{
+    setTodos([...todos, {
+      title:" market",
+      description:" bring vegetables, fruit"
+    }
+  ]);
+  }} title={title} description={description}> Add todos</button>
+  
+
     </>
-  )
-}
+     )
+      }
+  function Buttoncomponent({title, description}){
+    return(
+      <>
+     <h1>{title}</h1>
+     <h5> {description}</h5>
+      </>
+    )
+
+  }
 
 export default App
