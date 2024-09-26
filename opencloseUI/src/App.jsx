a5c1929c62faf1197b7a1adc109bb3972adfebf1
sujalcharati <sujalc024@gmail.com>
2024-09-26 23:53:59 +0530

    render

--- opencloseUI/src/App.jsx
+++ opencloseUI/src/App.jsx
@@ -3,16 +3,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-        
+      <div >
+        <Accordian title=" what is web 3 brief it "  section="  decentralization, blockchain technology, and user ownership of data"/> 
+
+        <Accordian title=" what is machine learning and its branches" 
+        section=" supervised , unsupervised ,semisupervised learning "
+        /> 
       </div>
     </>
   )
+}
+
+function Accordian({title,section}){ 
+  const [isopen, setIsopen] = useState(false);
+
+      return (
+       <div>
+
+           <div onClick={()=>{
+            setIsopen()
+           }}>
+              {title}
+           </div>
+           <div>
+              {section}
+           </div>
+      </div>
+        
+      )
 }
 
 export default App
