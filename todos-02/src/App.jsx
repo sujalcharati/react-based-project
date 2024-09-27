e09abf6314f9e140612f2708fd317ed50c2d7bc9
sujalcharati <sujalc024@gmail.com>
2024-09-27 23:34:40 +0530

    input setup

--- todos-02/src/App.jsx
+++ todos-02/src/App.jsx
@@ -1,15 +1,35 @@
+import { RecoilRoot, useRecoilValue } from 'recoil'
 import './App.css'
 
 function App() {
-
-  return (
-    <>
+   return (
+    <RecoilRoot>
       <div>
-        
-      
+        <Button/>
       </div>
-      
-    </>
+    </RecoilRoot>
+   )
+}
+
+function Button(){
+  const storetodos = useRecoilValue(todoatom);
+  return (
+    <div>
+      <div>
+        <input
+         type='text'
+         placeholder='title'/>
+       </div>
+      <div>
+        <input
+         type='text'
+         placeholder='description'/>
+        </div>
+
+        <button onClick={()=>{
+        
+        }}>submit</button>
+    </div>
   )
 }
 
