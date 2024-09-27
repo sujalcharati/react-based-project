f0ffd2c0fb64ba5b59e9544fe4a7472d698fbfdc
sujalcharati <sujalc024@gmail.com>
2024-09-27 13:10:17 +0530

    context api

--- counter-02/src/App.jsx
+++ counter-02/src/App.jsx
@@ -1,36 +1,45 @@
 import { useState } from 'react'
 
 import './App.css'
+import { useContext } from 'react';
+import { contextcontainer } from './context';
 
 
 function App(){
   const [count, setCount] = useState(0);
-
+// wrap any components that is using teleport data inside the context provider 
   return (
     <div>
-       <Counter count={count} setCount={setCount} />
+    {/* so here we teleporting count variable  */}
+      <contextcontainer.Provider value={count}>
       
+        <Counter  setCount={setCount} />
+      </contextcontainer.Provider>
+      
+        
     </div>
   )
 }
- 
-function Counter({count,setCount}){
+ // we can now see that there is no count var being passed through this Counter component and similarly we cando for the setCount as well 
+function Counter({setCount}){
   return <div>
-    {/* {count} */}
-    <Counterrender count={count}/>
-    <Buttons count={count} setCount={setCount} />
+    
+    <Counterrender />
+    <Buttons  setCount={setCount} />
   </div>
 
 }
 
- function Counterrender({count}){
+ function Counterrender({}){
+  const count =useContext(contextcontainer);// here it is using the usecontext for teleportation by the above provider
   return <div>
     {count}
   </div>
  }
  
 
-function Buttons({count,setCount}){
+function Buttons({setCount}){
+  const count = useContext(contextcontainer)//here it is using the usecontext for teleportation by the above provider
    return <div>
   <button onClick={()=>{
     setCount(count+1)
---
+++ counter-02/src/context.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+
+ export const contextcontainer = createContext(0);
