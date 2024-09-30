3487330929c164cdd71b76ea17c963329e598471
sujalcharati <sujalc024@gmail.com>
2024-09-30 17:44:48 +0530

    layout

--- profile_card/src/App.jsx
+++ profile_card/src/App.jsx
@@ -1,17 +1,49 @@
 
 
+import { RecoilRoot } from 'recoil'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        
-       
-      </div>
+   <div>
+    <RecoilRoot>
+    <Biggercomponent/>
+    </RecoilRoot>
+   </div>
       
     </>
+  )
+}
+
+function Biggercomponent(){
+  return (
+    <div
+      className=""
+      style={{
+        background: 'white',
+        width: '400px',
+        height: '450px',
+        border: '2px solid brown',
+        borderRadius: '4px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <img
+        src='https://media.licdn.com/dms/image/v2/D5603AQHoj_nP3nGQrg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1696093662536?e=1733356800&v=beta&t=STAKwPj0VSZ-wBZjqu9lItZqMxoQ01iSaN1Krr3X-d4'
+        style={{
+          borderRadius: '500px',
+          height: '84px',
+          marginTop: '-72px'
+        }}
+      />
+      <div style={{color:'black',fontWeight:'bold'}}> Sujal charati </div>
+      <div style={{color:'black',}}> Berhampur </div>
+    </div>
   )
 }
 
---
+++ profile_card/src/atoms.jsx/profile.jsx/card.jsx
@@ -0,0 +1,19 @@
+import { atom } from "recoil";
+
+const followersatom = atom({
+    key: "followersatom",
+    default :"80k"
+});
+
+const likesatom = atom({
+    key: "likesatom",
+    default :"803k"
+});
+
+const photosatom = atom({
+    key: "photosatom",
+    default :"1.4k"
+});
+
+
+
