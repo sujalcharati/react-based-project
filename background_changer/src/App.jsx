14f310af02a06e25df55a5d4f700d0965222be7e
sujalcharati <sujalc024@gmail.com>
2024-10-01 20:23:20 +0530

    error

--- background_changer/src/App.jsx
+++ background_changer/src/App.jsx
@@ -1,15 +1,70 @@
 
+import { useRecoilState } from 'recoil';
 import './App.css'
-
 function App() {
+  const [color,setColor]= useRecoilState('white');
 
+  const changecolor= ()=>{
+    setColor(color==='red'?'white':'red');
+   }
   return (
-    <>
-      <div>
-        
-     </div>
-    </>
-  )
+ 
+      
+  <div style={{background:'{ color}'}}>
+    
+    <div style={ { display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+       borderRadius:'9px',
+       }}>
+    <button onClick={ changecolor} style={{background:'red' }}>Red </button>
+    <button style={{background:'yellow'}}>Yellow </button>
+    <button style={{background:'blue'}}>Blue </button>
+    <button style={{ background:'pink'}}>pink </button>
+    <button style={{background:'green'}}>Green </button>
+    <button style={{background:'black'}}>Black </button>
+    <button style={{background:'defaul'}}>default </button>
+  </div>
+  </div>
+)
+     
+  
+  
 }
 
+
+
+ 
+
+
+
 export default App
+
+// import { useState, useEffect } from 'react'
+
+// import './App.css'
+
+// function App() {
+//   const [color, setColor] = useState('')
+
+//   useEffect(() => {
+//     document.body.style.background = color;
+//   }, [color])
+  
+//   const changeBG = (e) => {
+//     setColor(e.target.innerText.toLowerCase())
+//   }
+
+//   return (
+//     <div className='spanContainer'>
+//       <span onClick={changeBG}>Red</span>
+//       <span onClick={changeBG}>Yellow</span>
+//       <span onClick={changeBG}>Purple</span>
+//       <span onClick={changeBG}>Green</span>
+//       <span onClick={changeBG}>Blue</span>
+//       <span onClick={changeBG}>Orange</span>
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ background_changer/src/atoms/colors/atom.jsx
@@ -0,0 +1,36 @@
+import { atom } from "recoil";
+
+export const coloratom= atom({
+    key:"coloratom",
+    default:'white'
+})
+// import {atom } from Recoil
+
+// export const redatom = atom( {
+//     key:"redatom",
+//     default:" Red"
+// });
+// export const yellowatom = atom( {
+//     key:"yellowatom",
+//     default:" yellow"
+// });
+// export const blueatom  = atom( {
+//     key:"blueatom",
+//     default:" Blue"
+// });
+// export const pinkatom = atom( {
+//     key:"pinkatom",
+//     default:" Pink"
+// });
+// export const greenatom = atom( {
+//     key:"greenatom",
+//     default:" Green "
+// });
+// export const blackatom = atom( {
+//     key:"blackatom",
+//     default:" Black"
+// });
+// export const defaultatom = atom( {
+//     key:"defaultatom",
+//     default:" white "
+// });
