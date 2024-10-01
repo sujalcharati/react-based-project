7416dec8cd0d3dbaf5e051c53cad40a474e5c734
sujalcharati <sujalc024@gmail.com>
2024-10-01 23:48:09 +0530

    functionality

--- background_changer/src/App.jsx
+++ background_changer/src/App.jsx
@@ -1,35 +1,66 @@
-
-import { useRecoilState } from 'recoil';
+import { coloratom } from './atoms/colors/atom';
+import { RecoilRoot, useRecoilState } from 'recoil';
 import './App.css'
 function App() {
-  const [color,setColor]= useRecoilState('white');
-
-  const changecolor= ()=>{
-    setColor(color==='red'?'white':'red');
-   }
-  return (
  
-      
-  <div style={{background:'{ color}'}}>
-    
-    <div style={ { display:'flex',
-      justifyContent:'center',
-      alignItems:'center',
-       borderRadius:'9px',
-       }}>
-    <button onClick={ changecolor} style={{background:'red' }}>Red </button>
-    <button style={{background:'yellow'}}>Yellow </button>
-    <button style={{background:'blue'}}>Blue </button>
-    <button style={{ background:'pink'}}>pink </button>
-    <button style={{background:'green'}}>Green </button>
-    <button style={{background:'black'}}>Black </button>
-    <button style={{background:'defaul'}}>default </button>
-  </div>
+  return (
+ <div>
+      <RecoilRoot>
+         <Buttoncomponents/>
+      </RecoilRoot>
   </div>
 )
      
   
   
+}
+function Buttoncomponents(){
+  const [color,setColor]= useRecoilState(coloratom);
+
+  const redcolor = ()=>{
+    setColor(color ==='red'?'white':'red');
+   }
+  const yellowcolor = ()=>{
+    setColor(color ==='yellow'?'white':'yellow');
+   }
+  const bluecolor = ()=>{
+    setColor(color ==='blue'?'white':'blue');
+   }
+  const pinkcolor = ()=>{
+    setColor(color ==='pink'?'white':'pink');
+   }
+  const greencolor = ()=>{
+    setColor(color ==='green'?'white':'green');
+   }
+  const blackcolor = ()=>{
+    setColor(color ==='black'?'white':'black');
+   }
+  const defaultcolor = ()=>{
+    setColor(color ==='white'?'white':'white');
+   }
+  return(
+    <div>
+      
+  <div style={{backgroundColor: color, height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center',padding: '10px 20px', fontSize: '16px'}}>
+    
+    {/* <div style={ { display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+       borderRadius:'9px',
+      padding: '10px 20px',
+       fontSize: '16px'
+       }}> */}
+    <button onClick={redcolor} style={{background:'red' }}>Red </button>
+    <button onClick={yellowcolor}style={{background:'yellow'}}>Yellow </button>
+    <button onClick={bluecolor}style={{background:'blue'}}>Blue </button>
+    <button onClick={pinkcolor}style={{ background:'pink'}}>pink </button>
+    <button onClick={greencolor}style={{background:'green'}}>Green </button>
+    <button onClick={blackcolor}style={{background:'black',color:'white'}}>Black </button>
+    <button onClick={defaultcolor}style={{background:'default'}}>default </button>
+  </div>
+  </div>
+    // </div>
+  )
 }
 
 
