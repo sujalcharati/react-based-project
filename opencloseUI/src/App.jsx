e775dc63c9e31829d70f75849d77742a5e3acc5b
sujalcharati <sujalc024@gmail.com>
2024-10-02 17:41:29 +0530

    layout

--- opencloseUI/src/App.jsx
+++ opencloseUI/src/App.jsx
@@ -3,36 +3,60 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  
   return (
-    <>
+    
       <div >
-        <Accordian title=" what is web 3 brief it "  section="  decentralization, blockchain technology, and user ownership of data"/> 
-
-        <Accordian title=" what is machine learning and its branches" 
-        section=" supervised , unsupervised ,semisupervised learning "
-        /> 
-      </div>
-    </>
+        <Accordian 
+        title=" What is web 3 ?"
+        section="This ensures that the space between the flex items is distributed evenly. "/> 
+     </div>
+    
   )
 }
 
 function Accordian({title,section}){ 
   const [isopen, setIsopen] = useState(false);
-
+  const  toggleeffect =()=>{
+    
+        setIsopen(isopen==='true'?'opened':'closed');
+  }
       return (
-       <div>
-
-           <div onClick={()=>{
-            setIsopen()
-           }}>
+        <div>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width:'340px',
+            
+            color: 'black',
+            justifyContent: 'space-evenly',
+           
+          }}>
+            <div style={{ marginBottom: '10px', backgroundColor: '#7cd9d5',height:'55px',}}>
               {title}
-           </div>
-           <div>
-              {section}
-           </div>
-      </div>
-        
+              <span onClick={ toggleeffect} style={{ color:'brown'}}>+</span>
+              { 
+                (
+                  <div>
+                   <p>{section}</p> 
+                    </div>
+                )
+              }
+            </div>
+            <div style={{ marginBottom: '10px',backgroundColor: '#7cd9d5',height:'55px' }}>
+              what is machine learning ?
+              <span >+</span>
+            </div>
+            <div style={{ marginBottom: '10px',backgroundColor: '#7cd9d5' ,height:'55px'}}>
+              what is artificial language ?
+              <span>+</span>
+            </div>
+            <div style={{ marginBottom: '10px' ,backgroundColor: '#7cd9d5',height:'55px'}}>
+              what is Accordian component ?
+              <span>+</span>
+            </div>
+          </div>
+        </div>
       )
 }
 
