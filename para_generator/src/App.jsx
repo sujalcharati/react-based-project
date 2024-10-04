ccb65f8bfcc3032f9edb123108bc087507c323cd
sujalcharati <sujalc024@gmail.com>
2024-10-04 23:47:23 +0530

    ui

--- para_generator/src/App.jsx
+++ para_generator/src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
 
   return (
-  <div>
-     
-  </div>
+   
+      <div>
+            <Generator/>
+       </div>
+    
+   
+  )
+}
+
+function Generator(){
+  const [inputvalue,setInputvalue]=useState('');
+  const [para,setPara] =useState("");
+  const inputchange =(e)=>{
+    setInputvalue(Number(e.target.value));
+  }
+  const generateText= ()=>{
+
+    useEffect(()=>{
+    const newparagraph =
+  
+    },[para]);
+  }
+  return(
+    <div>
+      <h1> Para Generator</h1>
+      <input
+      type='text'
+      placeholder='Enter the number of words'
+      value={textsize}
+      onChange={ inputchange}
+      />
+      <button onClick={generateText}>Generate</button>
+    </div>
   )
 }
 
--- para_generator/src/main.jsx
+++ para_generator/src/main.jsx
@@ -3,5 +3,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
+ 
     <App />
+ 
 )
