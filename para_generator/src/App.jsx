892b56456ca9f369012f7d43c34c2b05827082c5
sujalcharati <sujalc024@gmail.com>
2024-10-05 11:49:12 +0530

    display

--- para_generator/src/App.jsx
+++ para_generator/src/App.jsx
@@ -15,28 +15,38 @@ function App() {
 }
 
 function Generator(){
-  const [inputvalue,setInputvalue]=useState('');
+  const [inputvalue,setInputvalue]=useState();
+ 
   const [para,setPara] =useState("");
-  const inputchange =(e)=>{
-    setInputvalue(Number(e.target.value));
-  }
-  const generateText= ()=>{
-
-    useEffect(()=>{
-    const newparagraph =
   
-    },[para]);
-  }
+  useEffect(()=>{
+    const arr = ['teckstack','portfolifo','groups','hackathons'];
+      const generateText= ()=>{
+
+       let newparagraph='';
+       for(let i=0;i<inputvalue;i++){
+        let noofwords= Math.floor(Math.random()*arr);
+        newparagraph+= arr[noofwords]+''
+       }
+      setPara(newparagraph.trim());
+    }
+    generateText();
+  },[inputvalue]);
+    
+  
   return(
     <div>
       <h1> Para Generator</h1>
       <input
-      type='text'
+      type='number'
       placeholder='Enter the number of words'
-      value={textsize}
-      onChange={ inputchange}
+      value={inputvalue}
+      onChange={ (e)=> setInputvalue(e.target.value)}
       />
       <button onClick={generateText}>Generate</button>
+      <div>
+        {para} 
+      </div>
     </div>
   )
 }
