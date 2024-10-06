35405511c611c22a85c32fed76ff3a4beb7e0fe5
sujalcharati <sujalc024@gmail.com>
2024-10-06 19:14:43 +0530

    basic setup

---
+++ login/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       
+      
+         
+      </div>
+      
+    </>
+  )
+}
+
+export default App
