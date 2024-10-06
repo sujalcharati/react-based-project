5f1983977ac5b1230812adc0b95167e989e1a93c
sujalcharati <sujalc024@gmail.com>
2024-10-06 22:14:01 +0530

    login

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -2,18 +2,25 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setNum] = useState('')
 
   return (
-    <>
-      <div>
-       
-      
-         
+   
+      <div> 
+        <Loginform/>
       </div>
-      
-    </>
+     )
+}
+function Loginform(){
+  return(
+<div>
+  <h1 style={{color:'white'}}>Login via OTP</h1>
+  <input
+  type='number'
+  value={num}
+  placeholder='enter the phone number'
+  onChange={e=> setNum(e.target.value)}/>
+</div>
   )
 }
-
 export default App
